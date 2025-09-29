c302f5fee81e6cb21b5867e917f3a2a6014706b5
HaoYu1 <hao_yu@ao.uscourts.gov>
2025-09-29 12:20:33 -0400

    switch to custom  AWS CLI plugin in container.yml

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <>
-      <h1>HI from Container App!</h1>
+      <h1>Hi from Container App!</h1>
       <hr />
       <MarketingApp />
     </>
