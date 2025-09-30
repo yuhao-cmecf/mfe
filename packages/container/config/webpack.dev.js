8aa98cfad93bf7e2bfc36ccf13382a787577a4f4
HaoYu1 <hao_yu@ao.uscourts.gov>
2025-09-30 08:44:40 -0400

    modified historyApiFallback in webpack js files

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -7,9 +7,7 @@ const devConfig = {
   mode: "development",
   devServer: {
     port: 80,
-    historyApiFallback: {
-      index: "/index.html",
-    },
+    historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
