43c72ea982d63b02dc2f21a04f0e38575e27faf9
HaoYu1 <hao_yu@ao.uscourts.gov>
2025-09-30 14:51:59 -0400

    fixed nagivation issues

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,26 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function MarketingApp() {
   const ref = useRef(null);
+  const browserHistory = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log("Container App notified of navigation (by Marketing App) to:", nextPathname);
+        const { pathname } = browserHistory.location;
+
+        // To avoid infinite loop
+        if (pathname !== nextPathname) {
+          browserHistory.push(nextPathname);
+        }
+      },
+    });
+
+    browserHistory.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = function (el) {
-  ReactDOM.render(<App />, el);
+// Mount function to start up the app
+const mount = function (el, { onNavigate, defaultHistory }) {
+  console.log("Mounting Marketing App");
+
+  // defaultHistory is only used for development in isolation
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log("Marketing App notified of navigation (by Container App) to:", nextPathname);
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
